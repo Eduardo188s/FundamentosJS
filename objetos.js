14039ac16c81955dfab75a3557a68daf281b803f
Francisco de Luis <deluisfloresfranciscoa@gmail.com>
2023-09-20 14:35:40 -0600

    12 Objetos de JavaScript

---
+++ objetos.js
@@ -0,0 +1,46 @@
+// Objetos en javascript
+/*
+objeto = {
+    key: value,
+    key: value
+}
+objeto.key 
+objeto['key']
+*/
+
+const subject = {
+    name: 'Aplicaciones web',
+    cuatri: 3,
+    professor: 'Francisco',
+    'classroom': 'Lab Redes I',
+    'total hours': 70,
+    weekHours: 5
+}
+
+console.log(subject);
+console.log(subject.name);
+console.log(subject.professor);
+console.log(subject['classroom']);
+console.log(subject['total hours']);
+
+subject.professor = 'Francisco de Luis';
+subject.carrer = 'TIADSM';
+console.log(subject);
+
+const student = {
+    name: 'Laura Alicia',
+    age: 21,
+    calificaciones: [10, 10, 8],
+    city: 'Apizaco'
+}
+
+const group = {
+    name: '4B',
+    students: [student, student, student],
+    subjects: [subject, subject, subject]
+}
+
+console.log(group);
+console.log(group.subjects[0].professor);
+console.log(group.students[1].calificaciones);
+console.log(group.students[1].calificaciones[0]);
